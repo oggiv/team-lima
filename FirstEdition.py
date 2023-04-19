53c1995800dcb8a25b94a4373bdd1eb978960e42
LeoisLazyy <91470214+LeoisLazyy@users.noreply.github.com>
2023-04-19 11:36:04 +0200

    Update FirstEdition.py

--- FirstEdition.py
+++ FirstEdition.py
@@ -1,16 +1,23 @@
 #SYNKA
-if(connection == true)
-light all lights in the same colour
+    Lista av connections som snackar med hubben
+    if(connection == true)
+    light all lights in the same colour
 #break down into pseudo code
+
+#HEALTH och HP
+     Health = 3 (3 liv)
+     
+#TIMER
+    Timer = 10; (t ex)
 
 #HANDSHAKE
 # COME BACK TO
-X = 0;
-while(X = 0)
-keep checking if connection is established
-if (connection established)
-X = 1;
-#break down into pseudo code
+    X = 0;
+    while(X == 0)
+    keep checking if connection is established
+    if (connection established)
+    X = 1;
+    #break down into pseudo code
 
 #COUNTDOWN
 if (sensor detects handhake) --> countdown == true;
@@ -32,16 +39,35 @@ while(true)
     Rundan startar, timer räknar ner 
     #break down into pseudo code
     
-#TIMER
-    
 
 #MATCHA
     Kolla att fysisk kontakt etableras
     Kolla handske 1 ID (färg) och sedan handske 2 ID
     Se om ID 1 = ID 2, då är handskakningen korrekt
     Flasha gröna lampor
+    Gå till lyckad runda
     Om nej
     Flasha röda lampor
+    if HP > 0, decrement HP with 1
+    Gå till ny runda
+    else GAME OVER
+    
+#LYCKAD RUNDA
+    Timer - 1;
+    Gå tillbaka till matcha par
+    
+#NY RUNDA
+    Resetta
+    Gå till matcha par
+    Spelat har börjat om igen
+
+#GAME OVER
+    Blinka lampor random
+    Spelet är över
+    Resetta till skaka hand 
+    Skaka hand för att börja om
+    
+    
     
     
     
