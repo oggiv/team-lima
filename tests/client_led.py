16800df0a1c06afbad795bfc64fe6a30c5cb12e0
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-19 16:39:13 +0200

    Added /tests and client test
    Socket test on the pico

---
+++ tests/client_led.py
@@ -0,0 +1,45 @@
+# client
+
+import network
+import socket
+from machine import Pin
+from wifi_config import ssid, password, port, server_address
+
+# turn of LED
+led = Pin('LED', Pin.OUT)
+led.off()
+
+# connect to wifi
+wlan = network.WLAN(network.STA_IF) # start interface as client on network
+wlan.active(True)
+wlan.connect(ssid, password)
+
+print('Connecting to WiFi...', end=' ')
+while not wlan.isconnected() and wlan.status() != network.WLAN.STAT_GOT_IP:
+    pass
+
+print('Connected.')
+print(wlan.ifconfig())
+print('')
+
+# open socket
+addr_interface = socket.getaddrinfo(server_address, port)[] # special address object for the socket. Is a 2D array
+addr_info = addr_interface[0][-1]
+
+print('Creating socket...', end=' ')
+sock = socket.socket()
+sock.connect(addr_info)
+print('Done.')
+
+# wait for response
+response = int(sock.recv(64)) # read 1 byte from buffer
+print('Received message: %s' % response)
+
+# evaluate message / turn on LED
+if response == 5:
+    print('LED turned on')
+    led.on()
+
+# end
+sock.close()
+    
---
+++ tests/wifi_config.py
@@ -0,0 +1,6 @@
+# parameters for the wifi
+
+ssid = 'test_rpi'
+password = '12345678'
+port = 80
+server_address = '192.168.4.1'
