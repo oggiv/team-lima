1abc084345f6290b38c271814f7cd659df4a7e6e
Eric <eric.lexenberg@gmail.com>
2023-04-19 16:42:04 +0200

    Added two versions of led countdown

--- FirstEdition.py
+++ FirstEdition.py
@@ -2,6 +2,13 @@
     Lista av connections som snackar med hubben
     if(connection == true)
     light all lights in the same colour
+    
+
+    #kanske går att ha en Connect method som tar
+    #en client som parameter?
+    #så ha en lista med unconnected och en lista
+    #med connected. flytt från ena till andra
+    #successivt
 #break down into pseudo code
 
 #HEALTH och HP
@@ -22,18 +29,63 @@
 #COUNTDOWN
 if (sensor detects handhake) --> countdown == true;
 
-while(true)
-    flash all lights
-    flash 3 lights
-    flash 2 lights
-    flash 1 light
-    flash 0 lights
-    break;
-    #break down into pseudo code
+#Väldigt simpelt exempel
+while true:
+  led1.on()
+  led2.on()
+  led3.on()
+  led4.on()
+  sleep(1)
+  led1.off()
+  sleep(1)
+  led2.off()
+  sleep(1)
+  led3.off()
+  sleep(1)
+  led4.off()
+  sleep(1)
+  break
+
+#kanske bättre med en rgb led som dimmas, färre pins
+#som går åt?
+from machine  import Pin
+from utime import sleep
+
+red = Pin(16, Pin.OUT)
+green = Pin(18, Pin.OUT)
+blue = Pin(20, Pin.OUT)
+
+while True:
+  red.value(0) # red
+  green.value(1)
+  blue.value(1)
+  sleep(1)
+  red.value(0) # yellow
+  green.value(0)
+  blue.value(1)
+  sleep(1)
+  red.value(1) # green
+  green.value(0)
+  blue.value(1)
+  sleep(1)
+  for i in range(8):
+      red.value(1) 
+      green.value(1)
+      blue.value(1)
+      sleep(0.1)
+      red.value(1)
+      green.value(0)
+      blue.value(1)
+      sleep(0.1)
+  
+  sleep(1)
+  
+#går åt lika många pins men snyggare kanske
+#sekvensen är röd -> gul -> grön sen blinka grön snabbt
 
 #MATCHA PAR
     Lista av handskar
-    bocka av vilka handksar som har vilken färg och därmed matchar
+    bocka av vilka handskar som har vilken färg och därmed matchar
     OBS! Max 2 handskar med samma färg
     Färg ID:ar par som ska kopplas fysiskt
     Rundan startar, timer räknar ner 
@@ -41,7 +93,8 @@ while(true)
     
 
 #MATCHA
-    Kolla att fysisk kontakt etableras
+    Kolla att fysisk kontakt etableras # Interrupt request?
+    #irq för den pin som sensorn är på?
     Kolla handske 1 ID (färg) och sedan handske 2 ID
     Se om ID 1 = ID 2, då är handskakningen korrekt
     Flasha gröna lampor
