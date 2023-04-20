5a2b6a6b139eeaa842172b43bfe7d82f119cd6e3
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-20 09:03:01 +0200

    Added our own server test
    This one interacts with the client_led test

--- tests/server_test.py
+++ tests/server_test.py
@@ -1,32 +1,42 @@
-import network
-import time
+# SERVER
+
+# start
 import socket
-from machine import Pin
+import network
+import gc
+# from wifi_config import ssid, password, port
 
-led = Pin("LED", Pin.OUT)
-led.off()
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect('RPI_PICO_AP', '12345678')
-# wait for connection
-while not wlan.isconnected() : 
-    print("Waiting to connect")
+# garbage collector
+gc.collect()
 
-wlan.status() # 3 is success
-wlan.ifconfig()
-print(wlan.ifconfig())
+ssid = 'test_rpi'
+password = '12345678'
+port = 80
+max_clients = 10 # maybe nr of unaccepted client?
 
-while True:
-    ai = socket.getaddrinfo("192.168.4.1", 80) # web server addr
-    addr = ai[0][-1]
+# starta wifi
+ap = network.WLAN(network.AP_IF) # create interface obj, ap = access point
+ap.config(essid=ssid, password=password) # , password=password   , security=network.WLAN.AUTH_OPEN
+ap.active(True)
+
+while ap.active() == False :
+    pass
+
+print('Interface activated.')
+print(ap.ifconfig())
+print('')
+
+# starta socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # (address family = IPv4, socket type = stream -> tcp)
+sock.bind(('', port)) # tuple (self, port)
+sock.listen(max_clients) # allow to accept max_clients amount of clients
+
+print('Waiting for connections...')
+# vänta på connection från klient
+while True :
+    conn, addr = sock.accept()
+    print('Connection accepted from %s' % str(addr))
     
-    # create socket, wait for instructions to light led
-    s = socket.socket()
-    s.connect(addr)
-    ss = int(s.recv(1))
-    print(ss)
-    
-    led.on()
-    
-    s.close()
-    time.sleep(0.2)
+    # skicka meddelande
+    conn.send(str(5)) # sending the nr 5
+    conn.close()
