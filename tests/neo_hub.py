964100a0afaf452d8fa423a82701faf90e5c5b75
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-24 15:21:03 +0200

    Added garbage collection to neo_hub

--- tests/neo_hub.py
+++ tests/neo_hub.py
@@ -1,7 +1,18 @@
 # neo_hub.py
 # WiFi access point test
 
+import gc
+import network
+import socket
+
+# Garbage collect to clear network interface settings
+gc.enable()
+gc.collect()
+
 # Start wifi
+
+
+
 # Listen for socket connections
 #  - Check for greeting
 # Send number
