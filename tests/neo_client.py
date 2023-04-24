03717c7a87021eb0e2d82002d5fd1918bf7470ba
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-24 15:19:28 +0200

    Set up new test files
    These will use socket.sendto()

---
+++ tests/neo_client.py
@@ -0,0 +1,8 @@
+# neo_client.py
+# WiFi connection and socket test
+
+# Connect to WiFi
+# Send greeting
+# Wait for response
+#  - Check response
+#   - Light LED
---
+++ tests/neo_hub.py
@@ -0,0 +1,7 @@
+# neo_hub.py
+# WiFi access point test
+
+# Start wifi
+# Listen for socket connections
+#  - Check for greeting
+# Send number
