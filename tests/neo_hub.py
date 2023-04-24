1842db5b83f592359551594708061d5e92061e5d
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-24 15:26:03 +0200

    Added WiFi initialization to neo_hub
    Temporary:
     ssid: test_rpi
     password: 12345678

--- tests/neo_hub.py
+++ tests/neo_hub.py
@@ -9,9 +9,14 @@ import socket
 gc.enable()
 gc.collect()
 
-# Start wifi
+# WiFi configuration
+ssid = 'test_rpi'
+password = '12345678'
 
-
+# Start WiFi
+ap = network.WLAN(network.AP_IF) # create interface object, ap means access point as in hub or hotspot
+ap.config(essid=ssid, password=password)
+ap.active(True)
 
 # Listen for socket connections
 #  - Check for greeting
