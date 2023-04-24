37efe396e0b6d63e4e6607bd0f2ba8dfd5597e78
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-24 15:57:07 +0200

    Added WiFi connectivity to neo_client

--- tests/neo_client.py
+++ tests/neo_client.py
@@ -1,7 +1,24 @@
 # neo_client.py
 # WiFi connection and socket test
 
+# --- WiFi ---
+# WiFi settings
+ssid = 'test_rpi'
+password = '12345678'
+
 # Connect to WiFi
+wlan = network.WLAN(network.STA_IF) # Start interface as client on network
+wlan.active(True)
+wlan.connect(ssid, password)
+
+# Wait for WiFi to connect
+while not wlan.isconnected() or wlan.status() >= 0:
+    pass
+
+# Get network information
+# The gateway is the hub's ip address
+client_ip, subnet, gateway, DNS = wlan.ifconfig()
+
 # Send greeting
 # Wait for response
 #  - Check response
