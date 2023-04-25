a0a7150c6f2a120e21e159d6617be9f39e3cc358
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-25 16:50:48 +0200

    Added some functions in wireless

--- wireless.py
+++ wireless.py
@@ -1,7 +1,14 @@
 # wireless.py
 # functions for communicating over WiFi
 
+import network
+import socket
+
 class Connection:
+    
+    def __init__(self, sock):
+        self._socket = sock
+    
     def send(message):
         # message = string || integer || float, else -> bruh
         #
@@ -18,12 +25,37 @@ class Connection:
         pass
 
 class Hub:
+    
+    def __init__(self):
+        self._accesspoint = network.WLAN(network.AP_IF)
+    
     def start_wifi(ssid, password):
         # ssid = string, name of network
         # password = string, password to network
         # return -> (my_ip, gateway_ip)
         #
         # note to self: enable hiding the network
+        
+        # create interface object, ap means access point as in hub or hotspot
+        _accesspoint.config(essid=ssid, password=password, hidden=True)
+        _accesspoint.active(True)
+        while not _accesspoint.active():
+            pass
+        
+    def stop_wifi():
+        # shut down WiFi access point
+        _accesspoint.active(False)
+        while _accesspoint.active():
+            pass
+    
+    def wifi_is_active():
+        # returns a boolean of the wifi status
+        return _accesspoint.active()
+    
+    def scan(ssid):
+        # scans for a hub network
+        # return -> True, if network exists
+        #        -> False, if there's no network
         pass
     
     def accept_connection():
@@ -33,13 +65,32 @@ class Hub:
         pass
     
 class Client:
+    
+    def __init__():
+        self._wlan = network.WLAN(network.STA_IF)
+    
     def connect_to_wifi(ssid, password):
         # ssid = string, name of network
         # password = string, password to network
-        # return -> (my_ip, gateway_ip)
+        # return -> (client_ip, gateway_ip)
         #
         # note to self: remember gateway_ip for connecting to hub later
-        pass
+        
+        # Start interface as client on network
+        _wlan.active(True)
+        _wlan.connect(ssid, password)
+
+        # Wait for WiFi to connect
+        while not _wlan.isconnected() and _wlan.status() >= 0:
+            pass
+
+        # Get network information
+        # The gateway is the hub's ip address
+        client_ip, subnet, gateway_ip, DNS = wlan.ifconfig()
+        return (client_ip, gateway_ip)
+        
+    def connected_to_wifi():
+        return _wlan.is_connected()
     
     def connect_to_hub():
         # return -> Connection, socket interface to hub
