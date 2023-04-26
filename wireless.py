041564ba253f42c5696b4d52ab64594df6116e89
Viggo <93593542+oggiv@users.noreply.github.com>
2023-04-26 11:29:28 +0200

    Added more comments to wireless.py

--- wireless.py
+++ wireless.py
@@ -6,16 +6,14 @@ import socket
 
 class Connection:
     
-    # interfaces the socket connection
-    
     def __init__(self, sock, address):
         self._socket = sock
         self.address = address
     
     def send(self, message):
-        # sends message (which must be a str, int or float) over socket
-        # message = string || integer || float, else -> TypeError
-        # return -> None
+        # message = string || integer || float, else -> bruh
+        #
+        # note to self: use type() to identify type and convert if necessary
         ty = type(message)
         if ty == str or ty == int or ty == float:
             self._socket.sendall(str(message).encode('UTF-8'))
@@ -23,42 +21,37 @@ class Connection:
             raise TypeError("must be str, int or float, not %s" % ty)
     
     def receive(self, bytes):
-        # reads [bytes] amount of bytes from socket
         # bytes = integer, amount of bytes to receive
         # return -> string
         message = self._socket.recv(bytes)
         return message.decode('UTF-8')
     
     def close(self):
-        # closes the socket connection
-        # return -> None 
+        # closes connection on this end
         self._socket.close()
     
 class Hub:
     
-    # interfaces functionality for hosting a wifi as a hub and accepting socket connections from clients
-    
     def __init__(self):
+        # configure interface as wifi access point
         self._accesspoint = network.WLAN(network.AP_IF)
-        self.gateway = ''
         self.ip = ''
 
-        # socket configuration
+        # Socket configuration
         self._socket = socket.socket()
         self._socket.bind(('0.0.0.0', 42)) # (address, port)
         self._socket.listen(5000)
         self._socket.settimeout(12.0)
     
     def start_wifi(self, ssid, password):
-        # configures and starts wifi access point
         # ssid = string, name of network
         # password = string, password to network
-        # return -> (client_ip, subnet_mask, hub_ip, dns_ip)
+        # return -> my_ip, gateway_ip
         #
-        # note to self: ssid isn't hidden
+        # note to self: hiding the ssid
         
         # create interface object, ap means access point as in hub or hotspot
-        self._accesspoint.config(essid=ssid, password=password)
+        self._accesspoint.config(essid=ssid, password=password) # , hidden=True
         self._accesspoint.active(True)
         while not self._accesspoint.active():
             pass
@@ -67,21 +60,17 @@ class Hub:
         return (self.ip, subnet, self.gateway, DNS)
         
     def stop_wifi(self):
-        # shuts down wifi access point
-        # return -> None
+        # shut down WiFi access point
         self._accesspoint.active(False)
         while self._accesspoint.active():
             pass
     
     def wifi_is_active(self):
-        # checks the wifi access point status
-        # return -> True, if a wifi is currently being hosted on the access point
-        #           False, if no wifi is currently being hosted on the access point
+        # returns a boolean of the wifi status
         return self._accesspoint.active()
     
     def scan(self, ssid):
-        ### WARNING: does not work
-        # scans for a wifi network with the given ssid
+        # scans for a hub network
         # return -> True, if network exists
         #        -> False, if there's no network
         network_list = self._accesspoint.scan()
@@ -91,19 +80,16 @@ class Hub:
         return False
     
     def accept_connection(self):
-        # listens for, createsa and returns a socket connection
         # return -> Connection, address
         #
-        # note to self: port is hard coded
-        #               timeout is not specified
+        # note to self: hard code port for now
         client_socket, address = self._socket.accept()
         return Connection(client_socket, address)
 
 class Client:
     
-    # interfaces functionality for connecting to a wifi as a client and opening socket connections to the hub
-    
     def __init__(self):
+        # configure interface as client on network
         self._wlan = network.WLAN(network.STA_IF)
         self._socket = socket.socket()
         self.gateway = ''
@@ -112,7 +98,7 @@ class Client:
     def connect_to_wifi(self, ssid, password):
         # ssid = string, name of network
         # password = string, password to network
-        # return -> (client_ip, subnet_mask, hub_ip, dns_ip)
+        # return -> client_ip, gateway_ip
         #
         # note to self: remember gateway_ip for connecting to hub later
         self._wlan.active(True)
@@ -120,7 +106,8 @@ class Client:
         while not self._wlan.isconnected() and self._wlan.status() >= 0:
             pass
 
-        # Get network information. The gateway is the hub's ip address.
+        # Get network information
+        # The gateway is the hub's ip address
         self.ip, subnet, self.gateway, DNS = self._wlan.ifconfig()
         if self.ip == '0.0.0.0':
             raise OSError("Could not connect to WiFi. Is the ssid correct?")
@@ -128,13 +115,9 @@ class Client:
             return (self.ip, subnet, self.gateway, DNS)
         
     def connected_to_wifi(self):
-        # checks if the client is currently connected to a wifi or not
-        # return -> True, if connected to a network
-        #           False, if not connected to a network
         return self._wlan.isconnected()
     
     def scan(self, ssid):
-        ### WARNING: does not work
         # scans for a hub network
         # return -> True, if network exists
         #        -> False, if there's no network
@@ -145,8 +128,8 @@ class Client:
         return False
     
     def get_connection(self):
-        # establishes and returns socket connection to hub
         # return -> Connection, socket interface to hub
-        sock = socket.socket()
-        sock.connect((self.gateway, 42))
-        return Connection(sock, self.gateway)
+        self._socket = socket.socket()
+        self._socket.connect((self.gateway, 42))
+        return Connection(self._socket, self.gateway)
+
