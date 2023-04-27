bd23098179e629e48d0cd107cf232602e658d047
Silv-ia <silvialinlu18@gmail.com>
2023-04-27 10:01:36 +0200

    Update Hubglove.py

--- Hubglove.py
+++ Hubglove.py
@@ -18,27 +18,33 @@ conn1 = hub.accept_connection()
 conn2 = hub.accept_connection()
 
 # receive ID + Handtype
-ID1 = int(conn1.receive(3))
-HAND1 = conn1.recieve(100)
-ID2 = int(conn2.receive(3))
-HAND2 = conn2.recieve(100)
+ID1 = int(conn1.receive(256))
+print(ID1)
+HAND1 = conn1.receive(100)
+print(HAND1)
+ID2 = int(conn2.receive(256))
+print(ID2)
+HAND2 = conn2.receive(100)
+print(HAND2)
 
 colourID = random.randint(1,5)
-    if colourID == 1:
-        colour == "green"
-    elif colourID == 2:
-        colour == "blue"
-    elif colourID == 3:
-        colour == "yellow"
-    elif colourID == 4:
-        colour == "purple"
-    elif colourID == 5:
-        colour == "cyan"
+if colourID == 1:
+    colour = "green"
+elif colourID == 2:
+    colour = "blue"
+elif colourID == 3:
+    colour = "yellow"
+elif colourID == 4:
+    colour = "purple"
+elif colourID == 5:
+    colour = "cyan"
         
 conn1.send(str(ID2))
 conn1.send(colour)
+conn1.send(1)
 conn2.send(str(ID1))
 conn2.send(colour)
+conn2.send(0)
 
 # close the connection
 conn1.close()
