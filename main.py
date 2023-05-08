9b72c5051db411a4a6b48cbe06ee9dba56ea5624
Pontus Hector <tjeckizz@gmail.com>
2023-05-08 09:18:04 +0200

    Eventual fix of dynamic hub/client glove

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import wireless
 from Hubglove import Hub
 from Clientglove import Client
 
-try:
-    Client('rpi_test')
-except OSError:
-    Hub('rpi_test')
+def connect(): # Connect to 'LAPTOP-4036F672'
+    try:
+        Client('LAPTOP-4036F672')
+    except OSError:
+        try:
+            Hub('LAPTOP-4036F672')
+        except OSError:
+            pass
+
+while True:
+    connect()
